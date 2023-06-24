1430b63f9bc7aaf7933c98e70960cc262ebfac3c
kris <hithard5@gmail.com>
2023-06-24 17:57:40 +0200

    setup output folder for generated DTS files

--- typed-scss-modules.config.ts
+++ typed-scss-modules.config.ts
@@ -2,4 +2,5 @@ export const config = {
     banner: "// THIS DTS FILE HAS BEEN GENERATED AUTOMATICALLY",
     nameFormat: "kebab",
     exportType: "default",
+    outputFolder: "__generated__",
 };
