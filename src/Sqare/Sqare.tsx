ff55507bc581225de7f51844356aeca72ac6420c
kris <hithard5@gmail.com>
2023-07-02 16:57:14 +0200

    clean main branch

--- src/Sqare/Sqare.tsx
+++
@@ -1,9 +0,0 @@
-interface SquareProps {
-    value: "X" | "O" | null;
-    index: number;
-    onSquareClick: (index: number) => void;
-}
-
-export function Square({ value, index, onSquareClick }: SquareProps) {
-    return <button onClick={() => onSquareClick(index)}>{value}</button>;
-}
